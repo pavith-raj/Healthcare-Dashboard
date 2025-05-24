081379046df800103b2525a14b31e18be9dfd4c9
Pavith Raj <pavithraj802@gmail.com>
2025-05-25 00:34:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './styles/App.css';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
-import DashboardMain from './components/DashboardMainContent';
+import DashboardMainConten from './components/DashboardMainContent';
 
 function App(){
   return (
     <div className="app-container">
-      <Sidebar />
-      <div className="main-content">
-        <Header />
-        <DashboardMain />
-
+      <Header />
+      <div className="content-area">
+        <Sidebar />
+        <DashboardMainConten />
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import '../styles/App.css';
+import {FaSearch, FaBell, FaPlus } from 'react-icons/fa';
+import avatar from '../assets/avatar.jpg';
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 style={{ margin: 0, color: "#2E3A59" }}>Welcome to Healthcare Dashboard</h1>
+        <div className="logo">
+            <span style={{ color: '#0AB3FF' }}>Health</span>
+            <span style={{ color: '#2E3A59' }}>care.</span>
+        </div>
+
+        <div className="search-bar">
+            <FaSearch className="search-icon" />
+            <input type="text" placeholder="Search" className="search-input" />
+        </div>
+
+        <div className="right-section">
+            <button className="add-button">
+                <FaPlus />
+            </button>
+
+            <FaBell className="notification-icon" />
+            <div className="user-profile">
+                <img src={avatar} alt="User Avatar" className="avatar" />
+                <span className="user-name">Dr. Sarah</span>
+            </div>
+        </div>
     </header>
   );
 };
